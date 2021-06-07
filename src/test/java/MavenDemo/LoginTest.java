5a49c945562aef52b06b6070408f1fec9cd09f2d
saurabh garg <sourabhgarg1@gmil.com>
2021-06-07 13:42:55 +0530

    Initial commit

---
+++ src/test/java/MavenDemo/LoginTest.java
@@ -0,0 +1,29 @@
+package MavenDemo;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.*;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class LoginTest {
+	
+	WebDriver driver;
+	
+	@Test
+	public void doLogin()
+	{
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		
+		driver.get("https://www.google.com/");
+		
+		driver.findElement(By.name("q")).sendKeys("Hello");
+	}
+
+}
